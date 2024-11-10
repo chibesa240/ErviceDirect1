9aecb60bc5f4cd29de55eb0bf5ff17ea20184d8a
chibesa240 <chibesamumbi15@gmail.com>
2024-11-10 13:58:05 +0200

    Create NavBar.js

---
+++ NavBar.js
@@ -0,0 +1,14 @@
+// src/components/NavBar.js
+import React from 'react';
+
+const NavBar = () => (
+  <nav className="top-nav">
+    <a href="#about">About Us</a>
+    <a href="#services">What Help Do You Need?</a>
+    <a href="#featured-services">Featured Services</a>
+    <a href="#apply-service">Want to Offer a Service?</a>
+    <a href="#contact">Contact Us</a>
+  </nav>
+);
+
+export default NavBar;
