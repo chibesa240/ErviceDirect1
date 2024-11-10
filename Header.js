c57acb143e4fb5594e1688bccad7b939252c0b5f
chibesa240 <chibesamumbi15@gmail.com>
2024-11-10 13:58:51 +0200

    Create Header.js

---
+++ Header.js
@@ -0,0 +1,14 @@
+// src/components/Header.js
+import React from 'react';
+
+const Header = () => (
+  <header className="header">
+    <div className="container">
+      <div className="logo">
+        <img src="logo.png" alt="Support Direct Logo" />
+      </div>
+    </div>
+  </header>
+);
+
+export default Header;
