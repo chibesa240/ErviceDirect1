a89f9e80ebfbbe6fa75e264ca1b6c303e9afa357
chibesa240 <chibesamumbi15@gmail.com>
2024-11-10 13:59:16 +0200

    Create About.js

---
+++ About.js
@@ -0,0 +1,13 @@
+// src/components/About.js
+import React from 'react';
+
+const About = () => (
+  <section id="about" className="about">
+    <div className="container">
+      <h2>About Us</h2>
+      <p>We connect people with skilled professionals across different fields. If you need help finding the right person, we’re here to make it easy and fast for you!</p>
+    </div>
+  </section>
+);
+
+export default About;
